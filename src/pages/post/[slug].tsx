df2fe53c5cb18812633eb79d61ec5e90194466f6
Vicente Mattos <vicentemattos1@hotmail.com>
2021-07-18 15:55:55 -0300

    Testes da page posts

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -2,7 +2,11 @@ import { format } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import Head from 'next/head';
+import { RichText } from 'prismic-dom';
+import { FaRegCalendar, FaRegClock, FaRegUser } from 'react-icons/fa';
+import Prismic from '@prismicio/client';
 
+import { useRouter } from 'next/router';
 import { getPrismicClient } from '../../services/prismic';
 
 import commonStyles from '../../styles/common.module.scss';
@@ -29,69 +33,105 @@ interface PostProps {
   post: Post;
 }
 
-export default function Post({ post }: PostProps) {
+export default function Post({ post }: PostProps): JSX.Element {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <h1>Carregando...</h1>;
+  }
+
+  const formatadedData = format(
+    new Date(post.first_publication_date),
+    'dd MMM yyyy',
+    { locale: ptBR }
+  );
+
   return (
     <>
       <Head>
         <title>{post?.data.title} | Blog</title>
       </Head>
 
-      <main style={{ backgroundImage: `url("${post?.data.banner.url}")` }}>
-        <article className={styles.container}>
-          <h1>{post?.data.title}</h1>
-          <time>{post?.first_publication_date}</time>
-          {post?.data.content.map(subContent => (
-            <div key={subContent.heading}>
-              <h2>{subContent.heading}</h2>
-              {subContent.body.map(description => (
-                <span>{description.text}</span>
-              ))}
+      <img src={post.data.banner.url} className={styles.banner} alt="" />
+
+      <main className={styles.container}>
+        <article className={styles.post}>
+          <h1>{post.data.title}</h1>
+          <div className={styles.postInfo}>
+            <time>
+              <FaRegCalendar size={14} />
+              {formatadedData}
+            </time>
+            <span>
+              <FaRegUser size={14} />
+              {post.data.author}
+            </span>
+            <span>
+              <FaRegClock size={14} />4 min
+            </span>
+          </div>
+          {post.data.content.map(data => (
+            <div className={styles.content} key={data.heading}>
+              <h2>{data.heading}</h2>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: RichText.asHtml(data.body),
+                }}
+              />
             </div>
           ))}
         </article>
       </main>
+
+      <main />
     </>
   );
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+  const prismic = getPrismicClient();
+  const posts = await prismic.query(
+    Prismic.predicates.at('document.type', 'post')
+  );
+
+  const paths = posts.results.map(post => {
+    return {
+      params: {
+        slug: post.uid,
+      },
+    };
+  });
   return {
-    paths: [],
-    fallback: 'blocking',
+    paths,
+    fallback: true,
   };
-  // const prismic = getPrismicClient();
-  // const posts = await prismic.query(TODO);
-  // TODO
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const { slug } = params;
-
   const prismic = getPrismicClient();
+  const { slug } = params;
   const response = await prismic.getByUID('post', String(slug), {});
-  // console.log(JSON.stringify(response, null, 2));
+
   const post = {
-    first_publication_date: format(
-      new Date(response.first_publication_date),
-      'd MMM yyyy',
-      {
-        locale: ptBR,
-      }
-    ),
+    uid: response.uid,
+    first_publication_date: response.first_publication_date,
     data: {
       title: response.data.title,
+      subtitle: response.data.subtitle,
+      author: response.data.author,
       banner: {
         url: response.data.banner.url,
       },
-      author: response.data.author,
-      content: response.data.content,
+      content: response.data.content.map(content => {
+        return {
+          heading: content.heading,
+          body: [...content.body],
+        };
+      }),
     },
   };
 
-  console.log(JSON.stringify(post, null, 2));
   return {
     props: { post },
   };
-
-  // TODO
 };
